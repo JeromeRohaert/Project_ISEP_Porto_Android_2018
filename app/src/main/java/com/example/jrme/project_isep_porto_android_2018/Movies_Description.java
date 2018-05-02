c70e7c9c897087c723f66236eff74a599b948ea4
JeromeRohaert <rohaert.jerome@gmail.com>
2018-05-02 18:42:07 +0200

    API Picasso added for pictures, beginning of the movie's description.

--- app/src/main/java/com/example/jrme/project_isep_porto_android_2018/Movies_Description.java
+++ app/src/main/java/com/example/jrme/project_isep_porto_android_2018/Movies_Description.java
@@ -1,10 +1,14 @@
 package com.example.jrme.project_isep_porto_android_2018;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,11 +19,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +39,10 @@ public class Movies_Description extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies__description);
         Intent intent = getIntent();
-        String id = intent.getStringExtra("id");
-        final TextView tv = findViewById(R.id.textView2);
+        final String id = intent.getStringExtra("id");
+        final TextView tv = findViewById(R.id.tv_title);
+        final ImageView iv = findViewById(R.id.iv_poster);
+        final TextView tv_description = findViewById(R.id.tv_description);
 
         RequestQueue queueT = Volley.newRequestQueue(this);
         String url = "https://api.themoviedb.org/3/movie/"+id+"?api_key=8d4eebc9735f52f03dbf6c13a652b5c7&language=en-US";
@@ -46,6 +56,10 @@ public class Movies_Description extends AppCompatActivity {
                             JSONObject description = new JSONObject(response);
                             String title = description.getString("title");
                             tv.setText(title);
+                            String url_poster_string = "http://image.tmdb.org/t/p/w185/"+ description.getString("poster_path");
+                            Picasso.with(Movies_Description.this).load(url_poster_string+"").into(iv);
+                            String s_descritpion = description.getString("overview");
+                            tv_description.setText(s_descritpion);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -62,4 +76,31 @@ public class Movies_Description extends AppCompatActivity {
         queueT.add(stringRequest);
 
     }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(Movies_Description.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+
 }
